aeef98c5068376a6aad6894639bbd890b29f5b1f
Marcel Breuer <breuer.marcel@outlook.com>
2024-11-19 08:32:43 +0100

    Bugfixes

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 // TODO: add auth sanctum
-Route::group(['prefix' => 'v1'], function () {
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::apiSingleton('user', App\Http\Resources\UserResource::class);
-        Route::apiResource('electricities', App\Http\Controllers\Api\ElectricityController::class);
-        Route::apiResource('gases', App\Http\Controllers\Api\GasController::class);
-        Route::apiResource('oils', App\Http\Controllers\Api\OilController::class);
-        Route::apiResource('waters', App\Http\Controllers\Api\WaterController::class);
-    });
+Route::group(['prefix' => 'v1', 'middleware' => 'auth:sanctum'], function () {
+    Route::apiSingleton('user', App\Http\Resources\UserResource::class);
+    Route::apiResource('electricities', App\Http\Controllers\Api\ElectricityController::class);
+    Route::apiResource('gases', App\Http\Controllers\Api\GasController::class);
+    Route::apiResource('oils', App\Http\Controllers\Api\OilController::class);
+    Route::apiResource('waters', App\Http\Controllers\Api\WaterController::class);
 });
